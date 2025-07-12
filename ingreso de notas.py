6fe3564253b067f45744dc8e40be3a07171f17cf
Usuario <hugocanizzabala@gmail.com>
2025-07-11 21:19:21 -0600

    ingrese2

--- ingreso de notas.py
+++ ingreso de notas.py
@@ -1,3 +1,6 @@
+from types import new_class
+
+
 class ingreso:
     def __init__(self,nombre,carnet,carrera,nota):
         self.nombre = nombre
@@ -7,3 +10,10 @@ class ingreso:
 
 lista_estu =[]
 
+nombre =input(f" ingrese nombre")
+carnet=input("ingrese carnet")
+carrera=input("ingrese carrera")
+nota=input("ingrese nota")
+cliente =new_class(nombre,carnet,carrera,nota)
+
+lista_estu.append(cliente)
