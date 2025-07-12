61be697e50aeba1ea8b9f82de877d12c131169f7
Usuario <hugocanizzabala@gmail.com>
2025-07-11 21:04:55 -0600

    clase estudiante

---
+++ ingreso de notas.py
@@ -0,0 +1,9 @@
+class ingreso:
+    def __init__(self,nombre,carnet,carrera,nota):
+        self.nombre = nombre
+        self.carnet = carnet
+        self.carrera = carrera
+        self.nota = nota
+
+lista_estu =[]
+
